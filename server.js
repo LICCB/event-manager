fce1a47de00d292ba223d36731b6bb150a3a8fbe
Jordan Handwerger <jordanhandwerger@yahoo.com>
2020-01-29 13:50:20 -0500

    Add basic create event form

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    res.render('index');
+  })
+
+app.get('/createEvent', function (req, res) {
+  res.render('createEvent');
+})
+
+app.post('/createEvent', function (req, res) {
+    console.log(req.body);
+    res.render('createEvent');
+  })
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
