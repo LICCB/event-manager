e33f004433ebcca7cb06fdaeb8507066bc024683
Christopher Landolfi <cjlando3297@gmail.com>
2020-02-05 21:37:41 -0500

    Added Registrant Signup Pages
    Created pages for public, private, and volunteer sign up for events.

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('index');
-})
+});
 
 app.get('/createEvent', function (req, res) {
   res.render('createEvent');
-})
+});
 
 app.post('/createEvent', function (req, res) {
     console.log(req.body);
     res.render('createEvent');
-})
+});
+
+app.get('/publicSignup', function (req, res) {
+  res.render('publicSignup');
+});
+
+app.post('/publicSignup', function (req, res) {
+    console.log(req.body);
+    res.render('publicSignup');
+});
+
+app.get('/privateSignup', function (req, res) {
+  res.render('privateSignup');
+});
+
+app.post('/privateSignup', function (req, res) {
+    console.log(req.body);
+    res.render('privateSignup');
+});
+
+app.get('/volunteerSignup', function (req, res) {
+  res.render('volunteerSignup');
+});
+
+app.post('/volunteerSignup', function (req, res) {
+    console.log(req.body);
+    res.render('volunteerSignup');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
