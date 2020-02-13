7c74fc18b4e10452b2860e897b3431d689f65f7a
ank97 <ankdave97@gmail.com>
2020-02-13 14:15:11 -0500

    default query + base selection

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const config = require('./config.json');
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-  host: config.database.host, 
-  user: config.database.user, 
+  host: config.database.host,
+  user: config.database.user,
   password: config.database.password,
   connectionLimit: 10,
   connectTimeout: 100000
@@ -17,6 +17,14 @@ async function queryAllUsers() {
   return users;
 }
 
+// return all users registered to event ID with default qualifications
+async function queryAllUsersDefaultSettings(eventID) {
+  let conn = await pool.getConnection();
+  let default_qualified_users = await conn.query("SELECT * FROM LICCB.events WHERE eventID='" + eventID + "' AND isAdult=true AND canSwim=true");
+  conn.release();
+  return default_qualified_users;
+}
+
 async function queryAllEvents() {
     let conn = await pool.getConnection();
     let events = await conn.query("SELECT * FROM LICCB.events");
@@ -26,9 +34,18 @@ async function queryAllEvents() {
 
 async function queryEventByID(eventID) {
   let conn = await pool.getConnection();
-  let event = await conn.query("SELECT * FROM LICCB.events WHERE eventID='" + eventID + "'")
+  let event = await conn.query("SELECT * FROM LICCB.events WHERE eventID='" + eventID + "'");
   conn.release();
   return event;
+}
+
+// initializes run selection process, NEED column for registrant with sign-up timestamp
+async function runSelection(eventID/*, canSwimFilter, CPRFilter, hasCPRCertFilter, isAdultFilter, */) {
+  let conn = await pool.getConnection();
+  // populate all contenders into new table
+  let returnSelectedRegistrants = await conn.query("BEGIN SELECT * INTO selectedMembers FROM LICCB.events WHERE eventID='" + eventID + "AND regStatus='Registered' AND isAdult='true' SELECT * FROM selectedMembers ORDER BY timestamp WHILE (@@RowCount > LICCB.eventID.capacity) BEGIN DELETE FROM selectedMembers END END");
+  conn.release();
+  return selectedMembers;
 }
 
 async function deleteEvent(id) {
@@ -42,19 +59,19 @@ async function insertEvent(event) {
   const id = uuidv4();
   const insertStmt = "INSERT INTO LICCB.events " +
                     "(eventID, eventName, manager, creatorID, " +
-                    "capacity, maxPartySize, privateEvent, startTime, " + 
-                    "endTime, staffRatio, published, eventNotes, eventMetadata) " + 
+                    "capacity, maxPartySize, privateEvent, startTime, " +
+                    "endTime, staffRatio, published, eventNotes, eventMetadata) " +
                 "VALUES(" +
-                    "'" + id + "', " + 
-                    "'" + event.eventname + "', " + 
+                    "'" + id + "', " +
+                    "'" + event.eventname + "', " +
                     "'" + event.manager + "', " +
                     "'1b671a64-40d5-491e-99b0-da01ff1f3341', " +
                     event.capacity + ", " +
-                    event.maxpartysize + ", " + 
+                    event.maxpartysize + ", " +
                     event.privateevent + ", " +
-                    "'" + event.startdate + " " + event.starttime + ":00', " +  
+                    "'" + event.startdate + " " + event.starttime + ":00', " +
                     "'" + event.enddate + " " + event.endtime + ":00', " +
-                    event.staffratio + ", " + 
+                    event.staffratio + ", " +
                     "1, " +
                     "'" + event.notes + "', " +
                     "NULL);";
@@ -65,21 +82,21 @@ async function insertEvent(event) {
 }
 
 async function createEventTable(id){
-  const createStmt = "CREATE TABLE LICCB.event" + id.replace(/-/g, "") + "(" + 
-                      "participantID   char(36) NOT NULL PRIMARY KEY, " + 
-                      "partyID         char(36) NOT NULL, " + 
+  const createStmt = "CREATE TABLE LICCB.event" + id.replace(/-/g, "") + "(" +
+                      "participantID   char(36) NOT NULL PRIMARY KEY, " +
+                      "partyID         char(36) NOT NULL, " +
                       "isAdult         BOOLEAN NOT NULL, " +
                       "canSwim         BOOLEAN NOT NULL, " +
                       "phone           VARCHAR(30) NOT NULL, " +
                       "email           VARCHAR(75) NOT NULL, " +
                       "emergencyPhone  VARCHAR(30) NOT NULL, " +
                       "emergencyName   VARCHAR(60) NOT NULL, " +
-                      "hasCPRCert      BOOLEAN NOT NULL, " + 
-                      "regStatus       ENUM('Registered', 'Not Selected', 'Standby', 'Selected', 'Checked In', 'No Show', 'Cancelled'), " + 
-                      "volunteer       BOOLEAN DEFAULT 0, " + 
-                      "FOREIGN KEY (`participantID`) " + 
+                      "hasCPRCert      BOOLEAN NOT NULL, " +
+                      "regStatus       ENUM('Registered', 'Not Selected', 'Standby', 'Selected', 'Checked In', 'No Show', 'Cancelled'), " +
+                      "volunteer       BOOLEAN DEFAULT 0, " +
+                      "FOREIGN KEY (`participantID`) " +
                           "REFERENCES `history` (`participantID`), " +
-                      "FOREIGN KEY (`partyID`) " + 
+                      "FOREIGN KEY (`partyID`) " +
                           "REFERENCES `history` (`participantID`));";
   let conn = await pool.getConnection();
   let create = await conn.query(createStmt);
@@ -88,16 +105,16 @@ async function createEventTable(id){
 }
 
 async function updateEvent(event, id) {
-  const update = "UPDATE LICCB.events " + 
+  const update = "UPDATE LICCB.events " +
                 "SET " +
-                  "eventName='" + event.eventname + "', " + 
-                  "manager='" + event.manager + "', " + 
+                  "eventName='" + event.eventname + "', " +
+                  "manager='" + event.manager + "', " +
                   "capacity=" + event.capacity + ", " +
-                  "maxPartySize=" + event.maxpartysize + ", " + 
+                  "maxPartySize=" + event.maxpartysize + ", " +
                   "privateEvent=" + event.participationtype + ", " +
-                  "startTime='" + event.startdate + " " + event.starttime + ":00', " +  
+                  "startTime='" + event.startdate + " " + event.starttime + ":00', " +
                   "endTime='" + event.enddate + " " + event.endtime + ":00', " +
-                  "staffRatio=" + event.staffratio + ", " + 
+                  "staffRatio=" + event.staffratio + ", " +
                   "creatorID='1b671a64-40d5-491e-99b0-da01ff1f3341', " +
                   "eventNotes='" + event.notes + "' " +
                 "WHERE eventID='" + id + "';"
