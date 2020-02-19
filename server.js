61049f2b81238a8fd6265aa89585a74e1baaf85d
senholmes <kholmes@stevens.edu>
2020-02-19 08:58:46 -0500

    Updated css files

--- server.js
+++ server.js
@@ -62,6 +62,33 @@ app.get('/deleteEvent/:id', async (req, res) => {
   res.redirect('/events');
 })
 
+app.get('/publicSignup', async (req, res) => {
+  res.render('signup/publicSignup', {title: "PublicSingup", events: await db.queryAllEvents()});
+});
+
+app.post('/publicSignup', function (req, res) {
+    console.log(req.body);
+    res.render('signup/publicSignup');
+});
+
+app.get('/privateSignup', async (req, res) => {
+  res.render('signup/privateSignup', {title: "PrivateSingup", events: await db.queryAllEvents()});
+});
+
+app.post('/privateSignup', function (req, res) {
+    console.log(req.body);
+    res.render('signup/privateSignup');
+});
+
+app.get('/volunteerSignup', async (req, res) => {
+  res.render('signup/volunteerSignup', {title: "VolunteerSingup", events: await db.queryAllEvents()});
+});
+
+app.post('/volunteerSignup', function (req, res) {
+    console.log(req.body);
+    res.render('signup/volunteerSignup');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
