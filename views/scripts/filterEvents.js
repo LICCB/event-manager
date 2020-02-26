8ee36d859f46b69772f1b377f859b6fd06a45853
kmorel <kmorel@stevens.edu>
2020-02-25 23:16:10 -0500

    Added jQuery DataTables to events page as a baseline.

--- views/scripts/filterEvents.js
+++ views/scripts/filterEvents.js
@@ -12,3 +12,13 @@ $('#cancelled').click(function(){
         $('.Cancelled').hide();
     }
 });
+
+$(document).ready( function() {
+  $('#eventsTable').DataTable({
+    'order': [7, 'asc'],
+    'columnDefs': [
+      {'targets': [0,1,2,3,6,11,14], "orderable": false, "searchable": false},
+      {'targets': [9,12,13], "orderable": false}
+    ]
+  });
+})
