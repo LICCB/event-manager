8106642f72670d618ebb765023b3e445d79414aa
ank97 <ankdave97@gmail.com>
2020-03-04 14:24:09 -0500

    script changes

--- views/scripts/filterParticipants.js
+++ views/scripts/filterParticipants.js
@@ -1,32 +1,27 @@
 $('#regStatus').click(function(){
-    var temp = "Registered";
     if($(this).prop("checked") == true){
-      console.log("CLICKED1");
-      $('tr').find('td:eq(0):contains('+temp+')').show();
-      $('RegStatus').show();
+      $('.Registered').show();
     } else if($(this).prop("checked") == false){
-      console.log("CLICKED2");
-      $('tr').find('td:eq(0):contains('+temp+')').hide();
-      $('RegStatus').hide();
+      $('.Registered').hide();
     }
 });
 
+$('#isAdult').click(function(){
+  if($(this).prop("checked") == true){
+    $('.adult').show();
+  } else if($(this).prop("checked") == false){
+    $('.adult').hide();
+  }
+});
 
-// $('#isAdult').click(function(){
-//   if($(this).prop("checked") == true){
-//     $('.isAdult').show();
-//   } else if($(this).prop("checked") == false){
-//     $('.isAdult').hide();
-//   }
-// });
 
-// $('#canSwim').click(function(){
-//   if($(this).prop("checked") == true){
-//     $('.canSwim').show();
-//   } else if($(this).prop("checked") == false){
-//     $('.canSwim').hide();
-//   }
-// });
+$('#canSwim').click(function(){
+  if($(this).prop("checked") == true){
+    $('.canSwim').show();
+  } else if($(this).prop("checked") == false){
+    $('.canSwim').hide();
+  }
+});
 
 // $('#hasCPRCert').click(function(){
 //   if($(this).prop("checked") == true){
