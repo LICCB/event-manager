7198ccadc3d17f5a92518acf7ec9d436f80c85b4
kmorel <kmorel@stevens.edu>
2020-04-09 14:49:30 -0400

    Added validation for unique party member names and emergency contacts not being in the party.

--- views/scripts/addParticipant.js
+++ views/scripts/addParticipant.js
@@ -5,9 +5,9 @@ $("#addpart").click(function () {
 
     var pLabel = $(`<h2>Party Member ${intId}</h2>`);
     var pFNameLabel = $(`<label for="part${intId}fname">Participant ${intId} First Name: </label>`);
-    var pFName = $(`<input   type="text" id="part${intId}fname" name="part${intId}fname" class="name" placeholder="Party Member First Name Here"><br>`);
+    var pFName = $(`<input   type="text" id="part${intId}fname" name="part${intId}fname" class="name part${intId} partFName" partId=${intId} placeholder="Party Member First Name Here"><br>`);
     var pLNameLabel = $(`<label for="part${intId}lname" >Participant ${intId} Last Name: </label>`);
-    var pLName = $(`<input   type="text" id="part${intId}lname" name="part${intId}lname" class="pname part${intId}" partId=${intId} placeholder="Party Member Last Name Here"><br>`);
+    var pLName = $(`<input   type="text" id="part${intId}lname" name="part${intId}lname" class="pname part${intId} partLName" partId=${intId} placeholder="Party Member Last Name Here"><br>`);
     var pAgeLabel = $(`<label for="part${intId}age" >Over 18? </label>`);
     var pAge = $(`<select   name= "part${intId}age" class="select"><option selected disabled hidden style="display: none" value=""></option><option value=0>No</option><option value=1>Yes</option></select><br>`);
     var pSwimLabel = $(`<label for="part${intId}swim" >Can they swim? </label>`);
@@ -21,7 +21,7 @@ $("#addpart").click(function () {
     var pPhoneLabel = $(`<label for="part${intId}phone" >Participant ${intId} Phone Number: </label>`);
     var pPhone = $(`<input type="text" id="part${intId}phone" name="part${intId}phone" class="pphone" placeholder="Party Member Phone Number Here"/><br>`);
     var pENameLabel = $(`<label for="part${intId}ename" >Participant ${intId} Emergency Contact Name: </label>`);
-    var pEName = $(`<input   type="text" id="part${intId}ename" name="part${intId}ename" class="name" placeholder="Party Member Emergency Contact Name Here"/><br>`);
+    var pEName = $(`<input   type="text" id="part${intId}ename" name="part${intId}ename" class="name eContactName" placeholder="Party Member Emergency Contact Name Here"/><br>`);
     var pEPhoneLabel = $(`<label for="part${intId}ephone" >Participant ${intId} Emergency Contact Phone Number: </label>`);
     var pEPhone = $(`<input   type="text" id="part${intId}ephone" name="part${intId}ephone" class="phone" placeholder="Party Member Emergency Contact Phone Number Here"/><br>`);
 
--- views/scripts/validator.js
+++ views/scripts/validator.js
@@ -1,4 +1,7 @@
 jQuery.validator.addClassRules({
+    volunteer: {
+        required: true,
+    },
     name: {
         required: true,
     },
@@ -32,22 +35,81 @@ jQuery.validator.addClassRules({
     },
     select: {
         required: true
+    },
+    eContactName: {
+        required: true,
+        testEContactName: true
     }
 });
 
 jQuery.validator.addMethod("testpName", function(value, element) {
-    // console.log(value, element);
-    let regFName = $("#regfirstname").val().toUpperCase();
-    let regLName = $("#reglastname").val().toUpperCase();
-    let partId = $(`#${element.id}`).attr('partId');
-    let partFName = $(`.part${partId}`)[0].value.toUpperCase();
-    let partLName = value.toUpperCase();
-    // console.log(partFName+partLName);
-    return this.optional(element) || (partFName + partLName) != (regFName + regLName);
-}, 'Participant name cannot equal registrant name.');
+    // Grab registrant information as you will always be checking against that
+    let regFName = $("#regfirstname").val().toUpperCase().replace(/\s/g,'');
+    let regLName = $("#reglastname").val().toUpperCase().replace(/\s/g,'');
+    // Grab current participant id from element
+    let currPartId = $(`#${element.id}`).attr('partId');
+    let currPartFName = $(`.part${currPartId}`)[0].value.toUpperCase().replace(/\s/g,'');
+    let currPartLName = value.toUpperCase().replace(/\s/g,'');
+    console.log('Registrant', regFName+regLName);
+    console.log('Current Participant', currPartFName+currPartLName);
+    // If the current participant shares name with registrant, invalid
+    if ((currPartFName + currPartLName) == (regFName + regLName)) {
+        return false;
+    }
+    else {
+        let firstNames = $(".partFName");
+        let lastNames = $(".partLName");
+        for (let i = 0; i < firstNames.length; i++) {
+            // Participant id starts at 1
+            if (i+1 != currPartId) {
+                let newPartFName = firstNames[i].value.toUpperCase().replace(/\s/g,'');
+                let newPartLName = lastNames[i].value.toUpperCase().replace(/\s/g,'');
+                console.log('Other Participant', newPartFName+newPartLName);
+                // If current participant shares name with another participant, invalid
+                if ((currPartFName + currPartLName) == (newPartFName + newPartLName)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}, 'Participant full name must be unique.');
+
+jQuery.validator.addMethod("testEContactName", function(value, element) {
+    // First grab registrant information, as you will always test against it
+    let regFName = $("#regfirstname").val().toUpperCase().replace(/\s/g,'');
+    let regLName = $("#reglastname").val().toUpperCase().replace(/\s/g,'');
+    console.log('Registrant', regFName+regLName);
+    // If the current element is the registrant emergency contact, check that against registrant name
+    // If they match, invalid
+    if ((element.id == 'regename') && (element.value.toUpperCase().replace(/\s/g,'') == (regFName + regLName))) {
+        return false;
+    }
+    let currEContactName = value.toUpperCase().replace(/\s/g,'');
+    console.log('Current Participant', currEContactName);
+    // If current emergency contact name equals registrant name, invalid
+    if ((regFName + regLName) == currEContactName) {
+        return false;
+    }
+    else {
+        let firstNames = $(".partFName");
+        let lastNames = $(".partLName");
+        for (let i = 0; i < firstNames.length; i++) {
+            let newPartFName = firstNames[i].value.toUpperCase().replace(/\s/g,'');
+            let newPartLName = lastNames[i].value.toUpperCase().replace(/\s/g,'');
+            console.log('Other Participant', newPartFName+newPartLName);
+            // If current emergency contact name equals a party member's name, invalid
+            if (currEContactName == (newPartFName + newPartLName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}, 'Emergency contact cannot be in your party.');
 
 $("#eventSignupForm").validate({
     errorClass: "validationError"
 });
 
 // https://jqueryvalidation.org/jQuery.validator.addMethod/
+// http://regexlib.com/DisplayPatterns.aspx?cattabindex=3&categoryId=4 
