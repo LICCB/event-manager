75398a06fcf80e871652145f2f33f1f5d5797efc
Christopher Landolfi <cjlando3297@gmail.com>
2020-04-30 19:01:13 -0400

    Add email confirmation to signup
    Adds the email confirmation module to the post route of the signup page.

--- server.js
+++ server.js
@@ -189,7 +189,9 @@ app.get('/eventSignup/:eventID/:volunteerStatus', async (req, res) => {
 });
 
 app.post('/eventSignup/:eventID/:volunteerStatus', async (req, res) => {
-  await db.insertParty(req.body, req.params.eventID, req.params.volunteerStatus);
+  const regID = await db.insertParty(req.body, req.params.eventID, req.params.volunteerStatus);
+  const reg = req.body;
+  mailer.sendConfirmationEmail(reg.regemail, reg.eventID, regID);
   res.redirect('/signup/signupThanks');
 });
          
